205a059f89bf4523bdd981066fa0ae8cfc959ddc
mikekavouras <kavourasm@gmail.com>
2016-12-12 16:56:59 -0600

    hard code OauthApplication.uid in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,5 @@
-Device.where(particle_id: ENV['PARTICLE_DEVICE_ID']).first_or_create
+Device.where(
+  particle_id: ENV['PARTICLE_DEVICE_ID'],
+  uid: "52f8a199ed929bf331b7a81a47b13f3347dc798c5d03ba4701e449d177810158"
+).first_or_create
 OauthApplication.where(name: "iOS App", redirect_uri: "https://lamp.engineering").first_or_create
